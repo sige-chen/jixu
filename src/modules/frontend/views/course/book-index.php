eeb2b604457c9fcb8803e91f3a8e6ba8500ba4f6
四格 <michaelluthor@163.com>
2020-09-28 11:22:25 +0800

    错误修改

--- src/modules/frontend/views/course/book-index.php
+++ src/modules/frontend/views/course/book-index.php
@@ -11,7 +11,7 @@ use yii\helpers\Url;
 <?php if ( null !== $online) : ?>
 <div class="row" style="background: white;padding: 20px;box-shadow: 5px 5px 5px 0px #e7e7e7;margin: 0; margin-bottom:20px;">
   <div class="col-md-3">
-    <img src="<?php echo FrontendAsset::getResUrl('images/course-book-online'); ?>" style="width: 100%;">
+    <img src="<?php echo FrontendAsset::getResUrl('images/course-book-online.jpg'); ?>" style="width: 100%;">
   </div>
   <div class="col-md-9">
     <h2 style="margin: 0;font-size: 24px;"><?php echo Html::encode($course->name); ?> 在线</h2>
